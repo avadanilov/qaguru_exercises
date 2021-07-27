9943d985314d0e6a95dbc39feb9ff2b28c1eabd0
avadanilov <avadanilov@yandex.ru>
2021-07-27 19:38:49 +0300

    add PO tests add Faker generated test data

---
+++ src/test/java/PracticeFormWithPOTests.java
@@ -0,0 +1,63 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import com.github.javafaker.Faker;
+
+public class PracticeFormWithPOTests {
+    RegistrationPage registrationPage = new RegistrationPage();
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            eMail = faker.internet().emailAddress(),
+            gender = "Male",
+            number = faker.numerify("8#########"),
+            subject = "maths",
+            hobby = "Reading",
+            address = faker.address().fullAddress(),
+            state = "NCR",
+            city = "Delhi",
+            picturePath = "./img/",
+            pictureFileName = "1.png";
+
+    String date = "28", //date of birth
+            month = "July",
+            year = "2005";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void positiveFillTest() {
+
+        registrationPage.openPage()
+                .typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeEmail(eMail)
+                .selectGender(gender)
+                .typeUserNumber(number)
+                .setDateOfBirth(date, month, year)
+                .setSubjects(subject)
+                .setHobby(hobby)
+                .uploadPicture(picturePath + pictureFileName)
+                .setCurrentAddress(address)
+                .setState(state)
+                .setCity(city)
+                .submit();
+
+
+        registrationPage.checkStudentName(firstName, lastName)
+                .checkStudentEmail(eMail)
+                .checkMobile(number)
+                .checkDateOfBirth(date, month, year)
+                .checkSubjects(subject)
+                .checkHobbies(hobby)
+                .checkPicture(pictureFileName)
+                .checkAddress(address)
+                .checkStateAndCity(state, city);
+    }
+}
